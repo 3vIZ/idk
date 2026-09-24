db6e7e1d8f293051b7f62a2724f40a89fad4ae6d
3vIZ <visionx1021@gmail.com>
2026-09-24 11:53:24 -0400

    cool hacks

--- main.ts
+++ main.ts
@@ -1,20 +1,11 @@
 input.onButtonPressed(Button.A, function () {
-    music.play(music.createSoundExpression(WaveShape.Noise, 5000, 0, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
-    music.play(music.createSoundExpression(WaveShape.Sine, 5000, 0, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.LoopingInBackground)
-    led.enable(true)
-    led.toggle(1, 4)
-    led.plot(2, 4)
-    led.plot(3, 4)
-    led.plot(0, 4)
-    led.toggle(4, 4)
-    led.toggle(4, 3)
-    led.plot(0, 3)
-    led.plot(1, 1)
-    led.plot(3, 1)
+    katakana.showString("hi")
 })
 input.onButtonPressed(Button.B, function () {
-    led.enable(false)
+    basic.showString("BREAK FINISHED.")
 })
 basic.forever(function () {
-	
+    basic.showString("PRESS A TO BREAK")
 })
+basic.showString("HELLO BOI")
+input.onButtonPressed(Button.AB), function () {}
